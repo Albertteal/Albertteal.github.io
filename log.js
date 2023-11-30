4aee0fe87adab55a8586d8cd4f6f2c2872c3301a
Albert_Pc_escritorio <albertteal@gmail.com>
2023-11-30 11:29:05 -0600

    se estiliza el boton de inciar de pantalla principal y se modifica el archivo de pruebas

--- log.js
+++ log.js
@@ -1,46 +1,59 @@
+var enteredPassword;
+var contrasenaInput;
+var NuevaContraParaEntrar;
+var NuevaContrasena;
+
 function verificarContrasena() {
 
 
-    const contrasenaInput = document.getElementById('contrasena');
-    const enteredPassword = contrasenaInput.value;
-  
-    if (enteredPassword === '7131') {
-      alert('Contraseña correcta. Accediste');
-      location.href = 'pantallaprincipal.html'; // Redirect to the main screen
-    } else {
+  contrasenaInput = document.getElementById('contrasena');
+  enteredPassword = contrasenaInput.value;
+
+  if (enteredPassword == '7131') {
+    alert('Contraseña correcta. Accediste');
+    location.href = 'pantallaprincipal.html'; 
+
+  } else if (enteredPassword == NuevaContraParaEntrar) {
+    alert('Contraseña correcta. Accediste');
+    location.href = 'pantallaprincipal.html'; 
+  }
+  else  {
+    alert('Contraseña incorrecta. Intente nuevamente.');
+  }
+
+}
+
+function PantallaRestablecerContrasena(){
+  location.href = 'restablecerContraseña.html';
+}
+
+
+function RestablecerContraseña(){
+
+  const contrasenadeRestablecimiento = document.getElementById('contrasena_de_restablecimiento');
+  const contrasenaIntroducidaparaEstablecer = contrasenadeRestablecimiento.value;
+
+  if (contrasenaIntroducidaparaEstablecer === '123') {
+      alert('Clave correcta. Contraseña reestablecida');
+      ReestableciendoContrasena();
+     
+    } else {  
       alert('Contraseña incorrecta. Intente nuevamente.');
     }
-  }
-  
-  function PantallaRestablecerContrasena(){
-    location.href = 'restablecerContraseña.html';
-  }
-
-  
-  function RestablecerContraseña(){
-
-    const contrasenadeRestablecimiento = document.getElementById('contrasena_de_restablecimiento');
-    const contrasenaIntroducidaparaEstablecer = contrasenadeRestablecimiento.value;
-
-    if (contrasenaIntroducidaparaEstablecer === '123') {
-        alert('Clave correcta. Contraseña reestablecida');
-        ReestableciendoContrasena();
-       
-      } else {  
-        alert('Contraseña incorrecta. Intente nuevamente.');
-      }
-      
-  }
-
-  function ReestableciendoContrasena(){
-
-    const NuevaContrasena = document.getElementById('Nueva_Contrasena');
-    const NuevaContraParaEntrar = NuevaContrasena.value;
-    location.href = 'index.html'; 
     
-    NuevaContraParaEntrar == contrasenaInput
-   
-  }
+}
+
+function ReestableciendoContrasena(){
+
+  NuevaContrasena = document.getElementById('Nueva_Contrasena');
+  NuevaContraParaEntrar = NuevaContrasena.value;
+  enteredPassword = NuevaContraParaEntrar;
+  alert(enteredPassword)
+  location.href = 'index.html'; 
+  
+  
+ 
+}
 
 
 
