6086e43c53b5498926038d6ef0a7dea5a40bd10b
albertteal <albertteal@gmail.com>
2023-11-29 22:14:10 -0600

    Se arregla el restablecimiento de contraseña

--- log.js
+++ log.js
@@ -4,7 +4,7 @@ function verificarContrasena() {
     const contrasenaInput = document.getElementById('contrasena');
     const enteredPassword = contrasenaInput.value;
   
-    if (enteredPassword === 'Valles') {
+    if (enteredPassword === '7131') {
       alert('Contraseña correcta. Accediste');
       location.href = 'pantallaprincipal.html'; // Redirect to the main screen
     } else {
@@ -16,18 +16,30 @@ function verificarContrasena() {
     location.href = 'restablecerContraseña.html';
   }
 
-
-  function RestablecerContraseña{
+  
+  function RestablecerContraseña(){
 
     const contrasenadeRestablecimiento = document.getElementById('contrasena_de_restablecimiento');
     const contrasenaIntroducidaparaEstablecer = contrasenadeRestablecimiento.value;
 
-    if (contrasenaIntroducidaparaEstablecer === 'Q@23faalz_24412#') {
+    if (contrasenaIntroducidaparaEstablecer === '123') {
         alert('Clave correcta. Contraseña reestablecida');
-        location.href = 'pantallaprincipal.html'; // Redirect to the main screen
-      } else {
+        ReestableciendoContrasena();
+       
+      } else {  
         alert('Contraseña incorrecta. Intente nuevamente.');
       }
+      
+  }
+
+  function ReestableciendoContrasena(){
+
+    const NuevaContrasena = document.getElementById('Nueva_Contrasena');
+    const NuevaContraParaEntrar = NuevaContrasena.value;
+    location.href = 'index.html'; 
+    
+    NuevaContraParaEntrar == contrasenaInput
+   
   }
 
 
