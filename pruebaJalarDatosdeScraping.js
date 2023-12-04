d181c03f32c9ad4a487f08e6a47be45dd30df5fd
Albert_Pc_escritorio <albertteal@gmail.com>
2023-12-03 20:11:17 -0600

    se logra enlazar el scraping de .test.js con el archivo .js para la logica de web scraping

--- pruebaJalarDatosdeScraping.js
+++ pruebaJalarDatosdeScraping.js
@@ -3,13 +3,13 @@
 const extraerInformacion = require('./extrayendoinfo.test');
 
 (async () => {
+  
   const extraccion = await extraerInformacion();
 
-  const boleta = extraerInformacion.boleta;
-  const nombre = extraerInformacion.nombre;
+  const boleta = extraccion.boleta;
+  const nombre = extraccion.nombre;
   console.log('nombre:', nombre);
   console.log('boleta:', boleta);
   
 })();
 
-module.exports = extraerInformacion;
