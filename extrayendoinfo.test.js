9b7d30642b7f18b7c33f3dbef27647404f0eda34
albertteal <albertteal@gmail.com>
2023-12-05 18:51:45 -0600

    se verifica el web scarping

--- extrayendoinfo.test.js
+++ extrayendoinfo.test.js
@@ -10,7 +10,7 @@ async function extraerInformacion() {
   });
 
   const page = await browser.newPage();
-  await page.goto('https://servicios.dae.ipn.mx/vcred/?h=41388f8616a528c6bac158748e1d07352e3b5111763b3dec1361f6b193c4', {
+  await page.goto('https://www.dae.ipn.mx/vcred/?h=e24d1d454ff2a75f71c29e72e0fd5fb275b5b5cdd521264d32dd541d1a3f', {
     waitUntil: 'networkidle0',
   });
  
